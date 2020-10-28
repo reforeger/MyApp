c1ef62eaa85e142160fb64011861dd4b3493680a
opilane <opilane@HP-CZC6297B8P>
2020-10-28 12:23:47 +0200

    Add project files.

---
+++ Elements_DM/Elements_DM/Tableview.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Elements_DM
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Tableview : ContentPage
+    {
+        public Tableview()
+        {
+            InitializeComponent();
+
+        }        
+    }
+}
